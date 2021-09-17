e31184bf18b7a5efa80998ff64fd6e38c0d4415b
Tushar Singh <tusharsingh.official@icloud.com>
2021-09-17 18:46:39 +0530

    netlify-form-updated

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-const handleSubmit = (event) => {
-    document.getElementById('ContactForm').reset();
-    successMessage();
-    setTimeout(closeSuccessMessage, 1500);
-}
+// const handleSubmit = (event) => {
+//     document.getElementById('ContactForm').reset();
+//     successMessage();
+//     setTimeout(closeSuccessMessage, 1500);
+// }
 
 // ----------- MENU
 
 const handleMenuOpen = () => document.getElementById('MenuContainer').className = 'openPopUp';
 const handleMenuClose = () => document.getElementById('MenuContainer').className = 'closePopUp';
-const successMessage = () => document.getElementById('SuccessMessage').className = 'openSuccessMessage';
-const closeSuccessMessage = () => document.getElementById('SuccessMessage').className = 'closeSuccessMessage';
+// const successMessage = () => document.getElementById('SuccessMessage').className = 'openSuccessMessage';
+// const closeSuccessMessage = () => document.getElementById('SuccessMessage').className = 'closeSuccessMessage';
